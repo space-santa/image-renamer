41ea406a7e62b2b8d416f7755b04d83a72e16c91
Claus Zirkel <claus.z@protonmail.com>
2017-06-16 09:26:01 +1000

    Added check for dnf.

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-sudo dnf install -y python3-exif
+if hash dnf 2>/dev/null; then
+    sudo dnf install -y python3-exif
+else
+    echo "Can't find dnf. Please make sure that python3-exif is available."
+fi
 
 sudo cp ReNamer.py /usr/local/bin/jpg-renamer
 sudo cp jpg-renamer.png /usr/share/icons/hicolor/64x64/apps/jpg-renamer.png
